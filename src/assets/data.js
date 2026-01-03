4016da7a4947efe28aa64ba92aadb69947ee1a84
Abhaynoor Singh <dhillonabhay71@gmail.com>
2026-01-04 00:09:31 +0530

    First commit - Quiz App

---
+++ src/assets/data.js
@@ -0,0 +1,42 @@
+export const data = [
+  {
+    question: "Which data structure works on the principle of FIFO?",
+    option1: "Stack",
+    option2: "Queue",
+    option3: "Tree",
+    option4: "Graph",
+    ans : 2,
+  },
+  {
+    question: "What does HTML stand for?",
+    option1: "Hyper Trainer Marking Language",
+    option2: "Hyper Text Markup Language",
+    option3: "Hyper Transfer Markup Language",
+    option4: "Hyperlink Text Management Language",
+    ans : 2,
+  },
+  {
+    question: "Which of the following is a JavaScript framework?",
+    option1: "Django",
+    option2: "Laravel",
+    option3: "React",
+    option4: "Spring",
+    ans : 3,
+  },
+  {
+    question: "What is the time complexity of binary search (average case)?",
+    option1: "O(n)",
+    option2: "O(n^2)",
+    option3: "O(log n)",
+    option4: "O(1)",
+    ans : 3,
+  },
+  {
+    question: "Which tag is used to link an external CSS file in HTML?",
+    option1: "<style>",
+    option2: "<css>",
+    option3: "<link>",
+    option4: "<script>",
+    ans : 3,
+  }
+];
